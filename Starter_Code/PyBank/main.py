b72b59ce64f33dd218e90fd5940557a2af474eea
ShoaibFarooqui <110314305+ShoaibFarooqui@users.noreply.github.com>
2023-01-17 13:58:28 -0600

    removed pseudocode, results output in one f string

--- Starter_Code/PyBank/main.py
+++ Starter_Code/PyBank/main.py
@@ -22,19 +22,20 @@ with open(file) as csv_file:
         current_month_profit = int(financial_reporting[1])
         net += current_month_profit
 
-        #calculate change in profit/loss each entry 
         change = current_month_profit - last_month_profit
         last_month_profit = current_month_profit
         change_ls.append(change)
         
-    #calculating average change (assuming no numpy package allowed
     total_months = len(month_counter)
     avg_change = st.mean(change_ls)
 
-    print(f'''
+    results = f'''
     Total Months: {total_months}
     Total: ${net}.00
     Average Changes: ${avg_change}
     Greatest increase in profits: {month_counter[change_ls.index(max(change_ls))]} (${max(change_ls)})
     Greatest decrease in profits: {month_counter[change_ls.index(min(change_ls))]} (${min(change_ls)})
-    ''')
+    '''
+
+print(results)
+    
