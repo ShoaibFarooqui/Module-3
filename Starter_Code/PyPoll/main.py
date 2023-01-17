034a50a280846fc6a6c9c57baad1b96b2b1a8e49
ShoaibFarooqui <110314305+ShoaibFarooqui@users.noreply.github.com>
2023-01-17 15:49:00 -0600

    coded total vote count

--- Starter_Code/PyPoll/main.py
+++ Starter_Code/PyPoll/main.py
@@ -19,5 +19,30 @@ with open(relative_path) as csv_file:
     csv_reader = csv.reader(csv_file)
     header = next(csv_reader)
 
+    total_id = []
+    total_county = []
+    total_vote = []
+
     for row in csv_reader:
-        print(row)
+        id = row[0]
+        county = row[1]
+        vote = row[2]
+        
+        total_vote.append(vote)
+    
+    vote_count = len(total_vote)
+    print(vote_count)
+
+
+'''
+Election Results
+-------------------------
+Total Votes: 369711
+-------------------------
+Charles Casper Stockham: 23.049% (85213)
+Diana DeGette: 73.812% (272892)
+Raymon Anthony Doane: 3.139% (11606)
+-------------------------
+Winner: Diana DeGette
+-------------------------
+'''
