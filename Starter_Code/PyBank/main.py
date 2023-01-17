60f07009ef5e838896ade60d785db7069d1d7a24
ShoaibFarooqui <110314305+ShoaibFarooqui@users.noreply.github.com>
2023-01-16 20:43:05 -0600

    Still finding the wrong P/L Change, made prettier

--- Starter_Code/PyBank/main.py
+++ Starter_Code/PyBank/main.py
@@ -13,26 +13,37 @@ Your analysis should align with the following results:
 '''
 import os
 import csv
+import statistics as st
 
 abs_path = os.path.dirname(__file__)
 file = os.path.join(abs_path, "Resources", "budget_data.csv")
 
-#header = Date, Gain/Loss
+#header = Date, Profit/Loss
 with open(file) as csv_file:
     csv_reader = csv.reader(csv_file)
     header = next(csv_reader)
+
     month_counter = []
-    profit = []
+    change_ls = []
     revenue = 0
+    initial_money = 0
+
     for budget in csv_reader:
         date = budget[0]
         month_counter.append(date)
 
         money = int(budget[1])
         revenue += money
-    
-    #calculating average change (assuming no numpy package allowed)
+        #calculate change in profit/loss
+        change = money - initial_money
+        initial_money = money
+        change_ls.append(change)
 
+    
+    #calculating average change (assuming no numpy package allowed
     total_months = len(month_counter)
-    print(f'{total_months} months')
-    print(f'${revenue}.00')
+    avg_change = st.mean(change_ls)
+    print(f'Period is {total_months} months')
+    print(f'Net Total is ${revenue}.00')
+    print(f'Changes in Profit/Loss over the entire period: ${sum(change_ls)}')
+    print(f'Average of all Profit/Loss changes: ${avg_change}')
