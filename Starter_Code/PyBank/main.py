637a756abd9ab41ec0dd97a1b65857d1e63262fd
ShoaibFarooqui <110314305+ShoaibFarooqui@users.noreply.github.com>
2023-01-16 19:55:00 -0600

    imported statistics package but mean_change off

--- Starter_Code/PyBank/main.py
+++ Starter_Code/PyBank/main.py
@@ -1,7 +1,7 @@
 '''
-The total number of months included in the dataset
+#The total number of months included in the dataset
 
-The net total amount of "Profit/Losses" over the entire period
+#The net total amount of "Profit/Losses" over the entire period
 
 The changes in "Profit/Losses" over the entire period, and then the average of those changes
 
@@ -13,6 +13,7 @@ Your analysis should align with the following results:
 '''
 import os
 import csv
+import statistics as st
 
 abs_path = os.path.dirname(__file__)
 file = os.path.join(abs_path, "Resources", "budget_data.csv")
@@ -22,17 +23,23 @@ with open(file) as csv_file:
     csv_reader = csv.reader(csv_file)
     header = next(csv_reader)
     month_counter = []
+    change = []
     revenue = 0
     for budget in csv_reader:
         date = budget[0]
-        money = budget[1]
+        money = int(budget[1])
 
         month = date.split("-")[0]
         day = date.split("-")[1]
         month_counter.append(month)
 
-        revenue += int(money)
+        revenue += money
+        change.append(money)
+    
+    #calculating average change (assuming no numpy package allowed)
+    mean_change = st.mean(change)
 
     total_months = len(month_counter)
     print(f'{total_months} months')
     print(f'${revenue}.00')
+    print(mean_change)
